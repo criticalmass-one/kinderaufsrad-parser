ae2d44de2a7d7e13767ed63ec3ccc14ca29176a2
Malte Hübner <maltehuebner@gmx.org>
2020-09-14 23:17:32 +0200

    Add more tests.

--- tests/DateTimeDetectorTest.php
+++ tests/DateTimeDetectorTest.php
@@ -25,6 +25,8 @@ class DateTimeDetectorTest extends TestCase
             ['19. September 2020, 11.00 Uhr', '2020-09-19 11:00'],
             ['20. September 2020, 15.00 Uhr', '2020-09-20 15:00'],
             ['20. September 2020, 15 Uhr', '2020-09-20 15:00'],
+            ['*26. September 2020, 14.00 Uhr', '2020-09-26 14:00'],
+            ['*26. September 2020, 14.00 Uhr  ', '2020-09-26 14:00'],
         ];
     }
 }
